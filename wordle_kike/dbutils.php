0b40e09edd902ccbeae8244bff745a1a769d69c8
EnriquePerezLazaro <113194712+EnriquePerezLazaro@users.noreply.github.com>
2023-03-06 18:30:57 +0100

    upd SACAR PALABRA test_dbutilskike.php

---
+++ wordle_kike/dbutils.php
@@ -0,0 +1,42 @@
+﻿<?php
+function conectarDB()
+{
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=db_wordle;charset=utf8mb4", "root", "");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    } catch (PDOException $ex) {
+        echo ("Error al conectar" . $ex->getMessage());
+    }
+}
+
+function getAllWords($conDB)
+{
+    $vectorTotal = array();
+    try {
+        $arr = array();
+        $sql = "SELECT * FROM palabras";
+        $stmt = $conDB->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $stmt->execute($arr);
+        while ($fila = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $vectorTotal[] = $fila;
+        }
+    } catch (PDOException $ex) {
+        echo ("Error al conectar" . $ex->getMessage());
+    }
+    return $vectorTotal;
+}
+
+function getPalabra($con, $id)
+{
+    try {
+        $sql = "SELECT nombre FROM palabras WHERE ID = id";
+        $stmt = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $stmt->execute(array(":id" => $id));
+        $fila = $stmt->fetch();
+    } catch (PDOException $ex) {
+        echo ("Error getPalabra" . $ex->getMessage());
+    }
+    return $fila;
+}
+?>
---
+++ wordle_kike/test_dbutilskike.php
@@ -0,0 +1,49 @@
+﻿<?php
+
+require_once("dbutils.php");
+$conexion = conectarDB();
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Injection</title>
+</head>
+
+<body>
+    <?php
+    require_once("dbutils.php");
+    if (isset($_POST["test"]))
+        var_export(getAllWords($conexion));
+
+    $arrayPalabras = [];
+    $palabras = getAllWords($conexion);
+    $arrayPalabras = $palabras;
+
+    $maximoPalabras = count($arrayPalabras);
+    echo " Palabras: " . count($arrayPalabras);
+    $min = 0;
+    $aleatorio = rand($min, $maximoPalabras);
+
+    echo "Palabra:" . $arrayPalabras[$aleatorio]["nombre"];
+
+    /*  $palabra = getPalabra($conexion, $aleatorio);
+    echo "Palabra sacada:" . $palabra; */
+
+
+
+
+    ?>
+    <form action="test_dbutils.php" method="post">
+        <input type="submit" value="Test" name="test" />
+    </form>
+</body>
+
+</html>
