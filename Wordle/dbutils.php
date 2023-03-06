4fbc36acff588995526b29a039f352083168df45
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-03-06 17:37:01 +0100

    update

---
+++ Wordle/dbutils.php
@@ -0,0 +1,32 @@
+﻿<?php
+    function conectarDB()
+    {
+        try
+        {
+            $db = new PDO("mysql:host=localhost;dbname=db_wordle;charset=utf8mb4","root","");
+            $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $db; 
+        }
+        catch (PDOException $ex)
+        {
+            echo ("Error al conectar".$ex->getMessage());
+        }
+    }
+
+    function getAllWords($conDB)
+    {
+        $vectorTotal = array();
+        try {
+            $arr = array();
+            $sql = "SELECT * FROM palabras";
+            $stmt = $conDB->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $stmt->execute($arr);
+            while ($fila = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $vectorTotal[] = $fila;
+            }
+        } catch (PDOException $ex) {
+            echo ("Error al conectar" . $ex->getMessage());
+        }
+        return $vectorTotal;
+    }
+?>
---
+++ Wordle/test_dbutils.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Injection</title>
+</head>
+<body>
+<?php 
+        require_once("dbutils.php");
+            if(isset($_POST["test"])) var_export(getAllWords(conectarDB())); 
+?>
+    <form action="test_dbutils.php" method="post">
+            <input type="submit" value="Test" name="test" />
+    </form>
+</body>
+</html>
